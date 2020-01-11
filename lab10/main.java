eeb88dca568aba45c9505c72deef79ff54c3ea44
beatriceblidariu <56226142+beatriceblidariu@users.noreply.github.com>
2020-01-12 00:12:54 +0200

    Add files via upload

---
+++ lab10/main.java
@@ -0,0 +1,110 @@
+import java.beans.XMLEncoder;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.TransformerFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class main {
+	
+	public String Author;
+	public String Book;
+	public String Chapter;
+	public String subChapter;
+	public int Paragraph;
+	public String Image;
+	public String Table;
+	
+	public main(String A, String B, String C, String sC, int P, String I, String T)
+	{
+		this.Author = A;
+		this.Book = B;
+		this.Chapter = C;
+		this.subChapter = sC;
+		this.Image = I;
+		this.Table = T;
+	}
+	
+	public static void main(String [] args) throws Exception 
+	{
+
+		
+		Section cap1 = new Section("Capitolul 1");
+		Paragraph p1 = new Paragraph("Paragraph 1");
+		cap1.add(p1);
+		Paragraph p2 = new Paragraph("Paragraph 2");
+		cap1.add(p2);
+		Paragraph p3 = new Paragraph("Paragraph 3");
+		cap1.add(p3);
+		Paragraph p4 = new Paragraph("Paragraph 4");
+		cap1.add(p4);
+		cap1.add(new ImageProxy("ImageOne"));
+		cap1.add(new Image("ImageTwo"));
+		cap1.add(new Paragraph("Some text"));
+		cap1.add(new Table("Table 1"));
+
+		cap1.setNewValue("CHAPTER 1");
+		p1.setNewValue("PARAGRAPH 2");
+		p2.setNewValue("PARAGRAPH 3");
+		cap1.setNewValue("CHAPTER 1.1");
+	}
+
+	public String getAuthor()
+	{
+		return Author;
+	}
+
+	public void setAuthor(String author)
+	{
+		Author = author;
+	}
+
+	public String getBook()
+	{
+		return Book;
+	}
+
+	public void setBook(String book)
+	{
+		Book = book;
+	}
+
+
+
+	public int getParagraph()
+	{
+		return Paragraph;
+	}
+
+	public void setParagraph(int paragraph)
+	{
+		Paragraph = paragraph;
+	}
+
+	public String getImage()
+	{
+		return Image;
+	}
+
+	public void setImage(String image)
+	{
+		Image = image;
+	}
+
+	public String getTable()
+	{
+		return Table;
+	}
+
+	public void setTable(String table)
+	{
+		Table = table;
+	}
+	
+	
+}
